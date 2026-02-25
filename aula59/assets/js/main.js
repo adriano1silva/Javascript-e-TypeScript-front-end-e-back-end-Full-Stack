bebe49fcb3c31ae9f3b6e39e7e6aab8b3fb8688e
Adriano Rafael Silva <adrianorafaelsilva@hotmail.com>
2026-02-25 18:15:55 -0300

    Calculadora com função construtora

---
+++ aula59/assets/js/main.js
@@ -0,0 +1,60 @@
+function Calculadora () {
+    this.display = document.querySelector('.display');
+    
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
+    };
+    
+    this.capturaEnter = () => {
+        document.addEventListener('keyup', e => {
+            if(e.key !== 'Enter') return;
+            this.realizaConta();
+        });
+    };
+
+    this.backSpace = () => {
+         document.addEventListener('keyup', e =>{
+            if(e.key !== 'Backspace') return;
+            this.del();
+        })
+    };
+
+    this.capturaCliques = () => {
+        document.addEventListener('click', event => {
+            const el = event.target;
+            if(el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if(el.classList.contains('btn-clear')) this.clear(el);
+            if(el.classList.contains('btn-del')) this.del(el);
+            if(el.classList.contains('btn-eq')) this.realizaConta(el);
+        });
+    };
+    
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText;
+        this.display.focus();
+    };
+    
+    this.del = el => this.display.value = this.display.value.slice(0, -1);
+    
+    this.clear = () => this.display.value = '';
+
+    
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
+            if(!conta){
+                alert('Conta Invalida')
+                return;
+            }
+            this.display.value = conta;
+        } catch (e) {
+            alert('Conta Invalida');
+            return;
+        }
+    };
+
+};
+
+const calculadora = new Calculadora();
+calculadora.inicia();
