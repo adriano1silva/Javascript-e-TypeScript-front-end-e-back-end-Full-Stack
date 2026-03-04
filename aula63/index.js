c73e56f8f8fc4ba2c0fe53e4c80b603a9698718f
Adriano Rafael Silva <adrianorafaelsilva@hotmail.com>
2026-03-04 14:28:08 -0300

    Método Splice

---
+++ aula63/index.js
@@ -0,0 +1,19 @@
+//                -5        -4       -3        -2          -1
+//                 0         1        2         3           4
+const nomes = ['Adriano', 'Silva', 'Saulo', 'Joaquim ', 'Pedro'];
+console.log(nomes);
+
+
+// nomes.splice(indice, delete, elem1, elemen2, element3);
+//pop
+// const removidos = nomes.splice(-1, 1);
+
+//shit 
+// const removidos = nomes.splice(0, 1);
+
+//push 
+// const adicionar = nomes.splice(nomes.length, 0, 'Jose');
+
+//unshift
+// nomes.splice(0, 0, 'Luiz', 'Silva' );
+
