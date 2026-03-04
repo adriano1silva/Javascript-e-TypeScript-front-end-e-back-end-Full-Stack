54caadd626be23a6f188cce74432f4bd7e2d1b84
Adriano Rafael Silva <adrianorafaelsilva@hotmail.com>
2026-03-04 13:14:21 -0300

    Revisando Arrays

---
+++ aula62/index.js
@@ -0,0 +1,36 @@
+//                 0         1         2
+// const nomes = ['Adriano', 'Maria', 'Eduardo'];
+// const nomes = new Array('Adriano', 'Maria', 'Eduardo');
+// nomes[2] = 'Joaquim';
+// delete nomes[2];
+// console.log(nomes);
+
+// Valor por referencia
+
+//                 0         1         2
+// const nomes = ['Adriano', 'Maria', 'Eduardo'];
+// const novo = [...nomes];
+
+// const removido = nomes.pop();
+// const removidoDois = nomes.shift();
+
+// nomes.push('Joaquina');
+// nomes.unshift('Saulo');
+
+
+// console.log(nomes.length);
+// console.log(removido);
+// console.log(removidoDois);
+// console.log(nomes);
+
+//                 0         1         2         3          4
+// const nomes = ['Adriano', 'Maria', 'Eduardo', 'Saulo', 'Wallace'];
+// const novo = nomes.slice(1, 3);
+// console.log(novo);
+
+const nome = ['Adriano','Rafael', 'Silva']
+// const nomes = nome.split(' ');
+const nomes = nome.join(' ');
+console.log(nomes);
+
+
